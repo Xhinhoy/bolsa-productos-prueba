df46f15f8bbae4fb9f3b6e6cd9c6e307448f4e9b
Xhinhoy <65037199+Xhinhoy@users.noreply.github.com>
2026-08-03 14:55:34 -0400

    chore: orquestacion docker de los cuatro servicios

---
+++ app/docker/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -e
+cd /var/www/html
+
+# APP_KEY persistida en el volumen: no se commitea y sobrevive reinicios
+KEYFILE=storage/app/.appkey
+[ -s "$KEYFILE" ] || php -r "echo 'base64:'.base64_encode(random_bytes(32));" > "$KEYFILE"
+export APP_KEY="$(cat "$KEYFILE")"
+
+php artisan migrate --force
+chown -R www-data:www-data storage bootstrap/cache
+
+exec "$@"
